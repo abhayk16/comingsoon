45ef899175e0980930dfdb8d3d158cfce255b961
abhayk16 <ggbois001@gmail.com>
2025-01-01 00:27:17 +0530

    updated date

--- client/scripts/bundle.js
+++ client/scripts/bundle.js
@@ -1,4 +1,4 @@
-var yourDate = new Date("2025-01-01T00:00:00").getTime();
+var yourDate = new Date("2025-06-01T00:00:00").getTime();
 
 function updateCountdown() {
     var now = new Date().getTime();
